0ec146bc10c9678db3f0cf4ebaa22ad33a9b6a8c
Sukhendu Sekhar Guria <sukhenduguria2017@gmail.com>
2024-01-15 18:39:37 +0530

    Add Populate Tag

--- populate.php
+++ populate.php
@@ -19,3 +19,78 @@
  * License:           GPL v2 or later
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
+
+use Faker\Factory;
+
+/**
+ * Main Populate Class.
+ */
+class Populate_CLI {
+
+	/**
+	 * Faker object.
+	 *
+	 * @var \Faker\Generator|Factory
+	 */
+	private \Faker\Generator|Factory $faker;
+	/**
+	 * Constructor.
+	 *
+	 * @since 1.0.0
+	 */
+	function __construct() {
+		require_once __DIR__ . '/vendor/autoload.php';
+		$this->faker = Faker\Factory::create();
+	}
+	/**
+	 * Populate tags.
+	 *
+	 * ## OPTIONS
+	 * --count=<number>
+	 *     The number of tags to create.
+	 *    ---
+	 *   default: 5
+	 *  ---
+	 * Example: wp populate tag --count=10
+	 *
+	 * @since  0.0.1
+	 */
+	public function tag($args, $assoc_args): void {
+		//Get the count of tags to create.
+		$count = 5;
+
+		if ( isset( $assoc_args['count'] ) ) {
+			$count = $assoc_args['count'];
+		}
+
+		//Progress bar.
+		$progress = \WP_CLI\Utils\make_progress_bar( 'Generating tags', $count );
+
+		//Loop through the number of tags to create.
+		for ($i=0; $i < $count; $i++) {
+			//Create a new tag.
+			wp_insert_term(
+				$this->faker->word,
+				'post_tag'
+			);
+
+			//Increment the progress bar.
+			$progress->tick();
+		}
+
+		//Complete the progress bar.
+		$progress->finish();
+	}
+}
+
+/**
+ * Registers our command when cli gets initialized.
+ *
+ * @since  1.0.0
+ * @author Scott Anderson
+ */
+function wds_cli_register_commands(): void {
+	WP_CLI::add_command( 'populate', 'Populate_CLI' );
+}
+
+add_action( 'cli_init', 'wds_cli_register_commands' );

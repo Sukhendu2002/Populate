b2cc2c7e97c1516cadc35f30469cf5a4c38e753d
Sukhendu Sekhar Guria <sukhenduguria2017@gmail.com>
2024-01-15 17:29:22 +0530

    Initial File

---
+++ populate.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Populate
+ *
+ * @package           Populate
+ * @author            Sukhendu Sekhar Guria
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Populate
+ * Plugin URI:        https://github.com/Sukhendu2002/Populate
+ * Description:       This is a plugin to populate the database with dummy data using wp-cli.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Sukhendu Sekhar Guria
+ * Author URI:        https://github.com/Sukhendu2002
+ * Text Domain:       populate
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
